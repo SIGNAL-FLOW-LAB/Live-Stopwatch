c4fd600fd167e9ec5377adb938a6d2f918836276
kobayashikantaro <kobayashikantaro@me.com>
2026-07-24 16:08:40 +0900

    v3.1.1 UI polish and Electric Blue app icon

--- App/SFFonts.swift
+++ App/SFFonts.swift
@@ -1,33 +1,36 @@
 import SwiftUI
 
-// MARK: - SIGNAL FLOW Fonts
-
 enum SFFont {
-
-    // タイトル
     static func title(_ size: CGFloat) -> Font {
-        .custom("Outfit", size: size)
-            .weight(.bold)
+        Font.system(
+            size: size,
+            weight: .semibold,
+            design: .default
+        )
     }
 
-    // 通常文字
     static func body(_ size: CGFloat) -> Font {
-        .custom("Outfit", size: size)
+        Font.system(
+            size: size,
+            weight: .regular,
+            design: .default
+        )
     }
 
-    // 数字（Show Timeと共通）
     static func timer(_ size: CGFloat) -> Font {
-        .system(
+        Font.system(
             size: size,
             weight: .bold,
-            design: .rounded
+            design: .default
         )
         .monospacedDigit()
     }
 
-    // 小さいラベル
     static func caption(_ size: CGFloat) -> Font {
-        .custom("Outfit", size: size)
-            .weight(.medium)
+        Font.system(
+            size: size,
+            weight: .regular,
+            design: .default
+        )
     }
 }
